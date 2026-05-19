1be3a0adb477f969021909487e7a319b66430bbd
ektabuneti123 <bunetikrishna@gmail.com>
2026-05-19 07:39:50 +0530

    Add files via upload

---
+++ pp1.py
@@ -0,0 +1,181 @@
+#First program
+print("Hello  world")
+
+print("EKTA")
+print("My name name is ekta")
+print("My age is 18.", "This is ekta.")
+print(35)
+print(89)
+print(35+89)
+name = "Ekta"
+age = 18
+price = 89.9
+print("My name is", name)
+print("My age is", age)
+print(price)
+age =90
+time = age
+print(time)
+print(type(name))
+print(type(age))
+print(type(price))
+age = 18
+old = True
+a = None
+print(type(age))
+print(type(old))
+print(type(a))
+
+#print sum
+a = 90
+b = 60
+diff = a - b
+print(diff)
+
+#Arithmetic operator
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+#relational operator
+a = 4
+b = 12
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a > b)
+print(a <= b)
+print(a < b)
+
+#Assignment operator
+num = 10
+#num = num + 10
+#num += 100
+#num *= 20
+#num -= 20
+#num /= 50
+#num **= 6
+num %= 7
+print("num : ", num)
+
+#logical operator
+print(not True)
+print(not False)
+
+a = 50
+b = 30
+print(not True)
+print(not (a >b))
+
+#And Operator
+value1 = True
+value2 = False
+#print("And operator:", value1 and value2)
+print("Or operator:", (a == b) or (a > b))
+
+#type conversion
+a = int("2")
+b  = 4.25
+
+sum = a + b
+print(type(a))
+
+print(a + b)
+
+
+a = 3.14
+a = str(a)
+print(type(a))
+print(a)
+
+name = input("Enter your name: ")
+print("Welcome ", name)
+
+val = input("Enter some value: ")
+
+print(type(val), val)
+
+val = int(input("Enter any value"))
+print(type(val), val)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+marks = float(input("Enter the total marks: "))
+
+print("welcome: ", name)
+print("Age: ", age)
+print("Your total marks:", marks)
+
+#Write a program to input 2 number & print their sum
+a = int(input("Enter any number: "))
+b = int(input("Enter any number: "))
+sum = a + b
+print(f"The Addition of given two number is {sum}")
+
+#WAP to input side of square & print its area
+side = int(input("Enter the area of side square: "))
+area = side * side
+print(f"The area of given side of square is: {area}")
+
+#WAP to input 2 floating point nnumber & print their average
+a = float(input("Enter any number: "))
+b = float(input("Enter any number: "))
+avg = a+b/2
+print(f"the averge of given two value is: {avg}")
+
+#WAP to input 2 int number a and b. print True if a is greater than or equal to b. If not print False
+a = int(input("Enter the First number: "))
+b = int(input("Enter the second number: "))
+print(a >= b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
